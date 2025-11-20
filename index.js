33f551e05b4f2b86d8498960ac54b937c6d40e41
Anuska86 <91799755+Anuska86@users.noreply.github.com>
2025-11-20 11:07:39 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,28 @@ import { catsData } from "./catsData.js";
 
 const emotionRadiosDiv = document.getElementById("emotion-radios");
 
+const getImageBtn = document.getElementById("get-image-btn");
+
 emotionRadiosDiv.addEventListener("change", highlightCheckedOption);
+
+//Get the cats
+
+function getMatchingCatsArray() {
+  const checkedRadio = document.querySelector('input[type="radio"]:checked');
+
+  if (checkedRadio) {
+    console.log(checkedRadio.value);
+    return checkedRadio.value;
+  }
+
+  return null;
+}
+
+getImageBtn.addEventListener("click", () => {
+  getMatchingCatsArray();
+});
+
+//Highlight the choice
 
 function highlightCheckedOption(e) {
   const currentHighlight = emotionRadiosDiv.querySelector(".highlight");
@@ -16,6 +37,8 @@ function highlightCheckedOption(e) {
   parentContainer.classList.add("highlight");
 }
 
+//Fetch the emotions
+
 function getEmotionsArray(cats) {
   const catsEmotions = [];
 
@@ -26,6 +49,8 @@ function getEmotionsArray(cats) {
   }
   return catsEmotions;
 }
+
+//Render the emotions
 
 function renderEmotionsRadios(cats) {
   const emotions = getEmotionsArray(cats);
